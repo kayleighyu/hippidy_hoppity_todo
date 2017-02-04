89d7ca630cd5cbec959d1a7c26aed979918ed9aa
Kayleigh Yu <kayleighyu@gmail.com>
2017-02-03 19:51:42 -0500

    added the ability to gather all of the input field data into a single todo object. The render function was updated to use those objects to output the list items for the todo list, simulating persistent memory. Lastly, wrote the createTodo factory function to produce todo objects from a task, category, and date.

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,40 @@
 var todoApp = {
   todos: [], //our data source/Store
   init: function(){
-    this.cacheDOM();
-    this.addEventListeners();
-    this.render();
+    todoApp.cacheDOM();
+    todoApp.addEventListeners();
+    todoApp.render();
   },
   cacheDOM: function(){
-    this.createButton = document.querySelector('#create');
-    this.taskInput = document.querySelector('#task');
-    this.list = document.querySelector('#list');
+    todoApp.createButton = document.querySelector('#create');
+    todoApp.taskInput = document.querySelector('#task');
+    todoApp.list = document.querySelector('#list');
+    todoApp.categoryInput = document.querySelector('#category');
+    todoApp.dateInput = document.querySelector('#date');
+    //queryselector selects anything based on its css selector. Typically finds ID bc it's unique.
   },
-  render: function(){
-    var listItemsFromTodos = this.todos
-                                 .map(function(todo){
-                                   return '<li>'+ todo +'</li>';
+  render: function(){ //puts things on the page
+    console.log(todoApp.todos);
+    var listItemsFromTodos = todoApp.todos
+                                 .map(function(todo){ //mapping is transforming, making a new array after the transformation
+                                   return `<li>${todo.task}:  (${todo.date})  [${todo.category}]</li>`;
                                  })
-                                 .join('');
-    this.list.innerHTML = listItemsFromTodos;
+                                 .join('');//the map creates an array with the above string in each position in array, then the join joins all of those strings together
+    todoApp.list.innerHTML = listItemsFromTodos;
   },
   addEventListeners: function(){
-    this.createButton.addEventListener('click', this.addTodo);
+    todoApp.createButton.addEventListener('click', todoApp.addTodo);
   },
   addTodo: function(){
-    var taskValue = todoApp.taskInput.value; //specific to input fields
-    todoApp.todos.push(taskValue);
+    var task = todoApp.taskInput.value; //specific to input fields
+    var date = todoApp.dateInput.value;
+    var category = todoApp.categoryInput.value;
+    var newTodo = createTodo(task, date, category);
+    todoApp.todos.push(newTodo);
+    todoApp.taskInput.value = "";
+    todoApp.dateInput.value = "";
+    todoApp.categoryInput.value = "";
     todoApp.render();
   }
 };
-console.log(todoApp);
 todoApp.init();
-console.log(todoApp);
